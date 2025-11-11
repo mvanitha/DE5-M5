0f8ba67d131260729f39bfe178fdde463462b86f
Vanitha Mascarenhas <mascarenhasvanitha@gmail.com>
2025-11-11 11:45:52 +0000

    VM: Day 2 AM1 work

--- src/cleanse_data.py
+++ src/cleanse_data.py
@@ -1,4 +1,5 @@
 import pandas as pd;
+import numpy as np;
 
 # Read source data files
 
@@ -8,24 +9,88 @@ in_systemcustomers_file = "C:\\Users\\Admin\\Desktop\\DE_M5\\gitrepos\\DE5-M5-20
 out_systembooks_file = "C:\\Users\\Admin\\Desktop\\DE_M5\\gitrepos\\DE5-M5-20251110\\Analysis\\03_Library Systembook.csv"
 out_systemcustomers_file = "C:\\Users\\Admin\\Desktop\\DE_M5\\gitrepos\\DE5-M5-20251110\\Analysis\\03_Library SystemCustomers.csv"
 
-df_books = pd.read_csv(in_systembooks_file, delimiter=',')
-df_customers = pd.read_csv(in_systemcustomers_file, delimiter=',')
 
-# Drop Rows from Books file with missing data
-cl_dfbooks = df_books.dropna(axis=0)
+def file_checker(filename, filepath):
+    print("Reading file {} from location {}".format(filename, filepath))
+    try:       
+        df_data = pd.read_csv(filepath, delimiter=',')
+        return df_data
+    except FileNotFoundError:
+        print("File not found.")
+    except pd.errors.EmptyDataError:
+        print("No data")
+    except pd.errors.ParserError:
+        print("Parse error")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+# Format headers
+def format_df_headers(data):   
+    return data.columns.str.replace(' ', '_')
+
+# Remove missing rows
+def remove_missing_rows(data):   
+    return data.dropna(axis=0)
 
 # Remove duplicates
-cl_dfbooks.Id.unique()
-#print(cl_dfbooks)
+def remove_duplicates(filename, data):    
+    print(f"Filename: {filename}")  
+    if filename == 'Customers':      
+        return data.Customer_ID.unique()
+    elif filename == 'Book':
+        return data.Id.unique()
+    else:
+        print(f"Not a valid data file")
+        return data
+    
+def calculate_borrowing_time(data): 
+    try:
+        #  Convert string to date
+        data.loc[:,'Book_checkout'] = data['Book_checkout'].str.replace(r'"', '')
+        data.loc[:,'Book_checkout'] = pd.to_datetime(data['Book_checkout'], format='mixed')
+        #data.head()
+        #data.info()
+    except Exception as e:
+        print(f"Error occurred: {e}")    
 
-#  Convert string to date
-cl_dfbooks['Book checkout'] = cl_dfbooks['Book checkout'].str.replace(r'"', '')
-pd.to_datetime(cl_dfbooks['Book checkout'], format="%d/%m/%Y", errors='coerce')
 
-# Drop Rows from Books file with missing data
-cl_df_customers = df_customers.dropna(axis=0)
+# Main block                         
+if __name__ == "__main__":
+    
+    # Read the Customers file
+    df_customers = file_checker("Customers", in_systemcustomers_file)
+    
+    # Format header columns
+    df_customers.columns = format_df_headers(df_customers)
 
-#Write data frames to the output folder
-cl_dfbooks.to_csv(out_systembooks_file);
-cl_df_customers.to_csv(out_systemcustomers_file);
-                
+    #rows_count =  len(df_customers)
+    print(f"Count of Rows in Customers file:{len(df_customers)}")
+
+    # Drop Rows from file with missing data
+    df_customers = remove_missing_rows(df_customers)    
+    print(f"Count of Rows in Customers file after dropping missing rows:{len(df_customers)}")
+
+    df_customers = remove_duplicates("Customers", df_customers)
+    print(f"Count of Rows in Customers file after removing duplicates:{len(df_customers)}")
+
+    
+    # Read the Books file
+    df_books = file_checker("Book", in_systembooks_file)
+    
+    # Format header columns
+    df_books.columns = format_df_headers(df_books)
+
+    # Drop Rows from file with missing data
+    print(f"Count of Rows in Books file:{len(df_books)}")
+    df_books = remove_missing_rows(df_books)    
+    print(f"Count of Rows in Books file after dropping missing rows:{len(df_books)}")
+
+    df_books = remove_duplicates("Book", df_books)
+    print(f"Count of Rows in Books file after removing duplicates:{len(df_books)}")
+
+    df_books = calculate_borrowing_time(df_books)
+    print(df_books)
+
+
+
+
